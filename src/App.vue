08946d58d7cc31d73ef1ed7de519c91c15e2c2f9
Bestian Tang <bestian@gmail.com>
2024-05-05 17:26:41 +0800

    close #3

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,12 @@ router-view
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #42b983 !important;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // 引入 Vue Router
 
+// 導入Semantic UI的CSS文件
+import 'semantic-ui-css/semantic.min.css'
+
 createApp(App).use(router).mount('#app')
