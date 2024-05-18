266789b6de01ebc6da4f7b04087a11de216a2a9d
Bestian Tang 大巴 <bestian@gmail.com>
2024-05-18 15:20:07 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,7 +9,7 @@
     .ui.two.column.stackable.row
       .ui.padded.segment.column
         h2.ui.header 自學2.0是什麼？
-        p 自學2.0是一個公益的尋友平台，可以依地圖的地理位置尋友，也可依照孩子的年紀，升旗者的興趣和可分享的專長來尋友
+        p 自學2.0是一個公益尋友平台，旨在透過地理位置、個人興趣及專長來協助用戶尋找朋友。平台的核心功能「互助旗」提供公開的自我介紹，讓人們在見面前能初步了解和認識對方。
         h2.ui.header 這是什麼單位在設計及維護的？
         p 本平台由非營利組織
           a(href="http://www.alearn.org.tw", target="_blank") 中華民國自主學習促進會
@@ -20,15 +20,16 @@
         p 為了讓有需要的朋友可以找到您，請至少一年更新一次您的互助旗。系統會自動隱藏一年以上未更新之互助旗。若兩年以上未更新，系統會刪除您的互助旗。
       .ui.padded.segment.column
         h2.ui.header 我的互助旗會是公開的嗎？能不能保密？
-        p 互助旗是完全公開的，您可以只留下願意公開的資訊
+        p 互助旗是完全公開的，您可以只留下願意公開的資訊，請參考
+          router-link(to="privacy-policy") 隱私權政策
         h2.ui.header 升起互助旗後可以修改嗎？
         p 可以，只要再次登入，點選「我的旗幟」即可修改
         h2.ui.header 升起互助旗後可以移除嗎？
         p 可以，您可以
           a(href="mailto:bestian@gmail.com") e-mail給管理員bestian@gmail.com
           | 手動移除
-        h2.ui.header 地圖介面的用意？
-        p 讓您可以就近尋找夥伴，例如:[宜蘭附近有沒有自學朋友呢]？ 看地圖就很清楚了
+        h2.ui.header 地圖介面有什麼用意
+        p 地圖讓您可以就近尋找夥伴，例如:[宜蘭附近有沒有自學朋友呢]？ 看地圖就很清楚了。
 
 </template>
 
