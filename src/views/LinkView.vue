c1f23365b54ac76026cec4815d59046e84ba92a9
leechiuhui <107933615+leechiuhui@users.noreply.github.com>
2024-07-09 16:49:01 +0800

    embeded 星河計畫 - 自學生經驗匯流 build 2024-7-9 16:48PM

--- src/views/LinkView.vue
+++ src/views/LinkView.vue
@@ -6,7 +6,14 @@
       target="_blank", rel="noopener noreferrer")
       | 1. 星河計畫 - 自學生經驗匯流
 
-
+      iframe(
+      src="https://xw9nw431joj2qm.embednotionpage.com/a0a32d5d4ddf43b28ae6a73c073e1c8c?v=72e87b01f9844b849d442458fe46aa04"
+  style="width: 100%; height: 500px; border: none !important; padding: 0;"
+      width="100%"
+      height="800px"
+      frameborder="0"
+      allowfullscreen
+    )
 </template>
 
 <style type="text/css" scoped>
