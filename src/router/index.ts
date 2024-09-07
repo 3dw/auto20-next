3d4fc559792234ff17b147701c44d1489dd35ce3
Bestian Tang <bestian@gmail.com>
2024-09-07 16:28:20 +0800

    close #173
    果然是history mode造成的，把它改成惰性載入就解決了

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import(/* webpackChunkName: "home" */ '../views/HomeView.vue')
   },
   {
     path: '/qr/:uid',
