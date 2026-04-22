172ab1183478d981339f8ccd830a8fe69e9eb087
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-04-22 12:45:46 +0800

    延續從過去一直活絡到現在的pol.is調查，並加入報告頁連結

--- src/views/PolisView.vue
+++ src/views/PolisView.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 .home
-  div.polis(data-conversation_id='5azfwmdsb6')
+  a.ui.teal.button(href='https://www.alearn.org.tw/report.html', target='_blank', rel='noopener noreferrer') 看近期完整報告
+    i.arrow.right.icon
+  div.polis(data-conversation_id='89bzf78kbn')
 
 </template>
 
@@ -16,7 +18,7 @@ export default defineComponent({
       var script = document.createElement('script');
       script.id = 'polis-script'
       script.async = true;
-      script.src = 'https://polis.tw/embed.js';
+      script.src = 'https://pol.is/embed.js';
       // 將 script 元素添加到 document 的 head 中
       document.head.appendChild(script);
     }, 100); // 延遲時間為 1000 毫秒，即 1 秒
